976f13ebcba7574622f00e0465d191ea4c63e88d
Letder40 <74748354+Letder40@users.noreply.github.com>
2022-02-01 23:25:40 +0100

    No voy a trabajar mas hoy asique commit XD

--- main/nivel 1/js/cambio_de_escenario.js
+++ main/nivel 1/js/cambio_de_escenario.js
@@ -1,3 +1,15 @@
+const escenario = $('.grid_escenario')
+const estanteria = $('.estanteria')
+//x defecto
+escenario.show()
+estanteria.hide()
+
+//cambios
 function openestanteria(){
-    
+    escenario.hide()
+    estanteria.show()
+}
+function estanteriaback(){
+    escenario.show()
+    estanteria.hide()
 }
