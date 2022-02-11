0f3d49c00f2cecf07e131a652d415c0246c3d2ac
Diego H. R <diegoherred@gmail.com>
2022-02-11 13:40:25 +0100

    Mi commit (ADRI)

--- main/js/2bleclick.js
+++ main/js/2bleclick.js
@@ -1,44 +1,48 @@
-//en html debe ser llamado en el elemento appicon como ondblclick="dblclick" 
-//IMPORTANTE LE DEBES AÑADIR EL ID DENTRO DE LA FUNCIÓN DE LO QUE QUIERES QUE SE MUESETRE AL HACER DOBLE CLICK 
-function dblclick(id) {
-    const element = document.getElementById(id)
-    element.style = "visibility: visible"
-    element.style = "z-index: 10"
-    console.log("funciono")
-}
-//este para el borón de cerrar funciona igual pero hace lo contrario
-function close(id) {
-    const element = document.getElementById(id)
-    element.style = "visibility: hidden"
-    element.style = "z-index: -10"
-}
-//en html debe ser llamado en el elemento appicon como onclick="unclick"
-function unclick(id) {
-    const elemento = document.getElementById(id)
-    if (elemento != document.getElementById('app-4 blocked')) {
-        elemento.style.cssText = "background-color: rgba(255, 255, 255, 0.199); border: 1px dotted white;"
-    } else {
-        elemento.style.cssText = "background-color: rgba(255, 0, 0, 0.199); border: 1px dotted red;"
-    }
-    setTimeout("reset()", 100)
-}
+getElementById("igoogle")
+getElementById("ifilesexplorer)
+        getElementById("i") getElementById("i")
 
-function reset() {
-    const body = document
-    body.onclick = function() {
-        //cuando añadas mas apps al html añadelas aqui tambien
-        const app1 = document.getElementById("app-1 trash")
-        const app2 = document.getElementById("app-2 chrome")
-        const app3 = document.getElementById("app-3 files-explorer")
-        const app4 = document.getElementById("app-4 blocked")
+        //en html debe ser llamado en el elemento appicon como ondblclick="dblclick" 
+        //IMPORTANTE LE DEBES AÑADIR EL ID DENTRO DE LA FUNCIÓN DE LO QUE QUIERES QUE SE MUESETRE AL HACER DOBLE CLICK 
+        function dblclick(id) {
+            const element = document.getElementById(id)
+            element.style.cssText = "visibility: visible"
+            element.style.cssText = "z-index: 100"
+            console.log(element + "funciono")
+        }
+        //este para el borón de cerrar funciona igual pero hace lo contrario
+        function close(id) {
+            const element = document.getElementById(id)
+            element.style = "visibility: hidden"
+            element.style = "z-index: -100"
+        }
+        //en html debe ser llamado en el elemento appicon como onclick="unclick"
+        function unclick(id) {
+            const elemento = document.getElementById(id)
+            if (elemento != document.getElementById('app-4 blocked')) {
+                elemento.style.cssText = "background-color: rgba(255, 255, 255, 0.199); border: 1px dotted white;"
+            } else {
+                elemento.style.cssText = "background-color: rgba(255, 0, 0, 0.199); border: 1px dotted red;"
+            }
+            setTimeout("reset()", 100)
+        }
 
-        app1.style.cssText = ""
-        app2.style.cssText = ""
-        app3.style.cssText = ""
-        app4.style.cssText = ""
+        function reset() {
+            const body = document
+            body.onclick = function() {
+                //cuando añadas mas apps al html añadelas aqui tambien
+                const app1 = document.getElementById("app-1 trash")
+                const app2 = document.getElementById("app-2 chrome")
+                const app3 = document.getElementById("app-3 files-explorer")
+                const app4 = document.getElementById("app-4 blocked")
+
+                app1.style.cssText = ""
+                app2.style.cssText = ""
+                app3.style.cssText = ""
+                app4.style.cssText = ""
 
 
-        //fin del espacio de añadir apps
-        body.onclick = ""
-    }
-}
+                //fin del espacio de añadir apps
+                body.onclick = ""
+            }
+        }
