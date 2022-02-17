60d78f4efcdf81dc7b83b38d6a29b1c7c7150a08
Letder40 <74748354+Letder40@users.noreply.github.com>
2022-02-17 20:25:00 +0100

    sugerencia de reestructuración a switch terminal

--- main/js/terminal2.js
+++ main/js/terminal2.js
@@ -1,3 +1,4 @@
+
 //llamada1
 document.addEventListener("keydown", next)
 
@@ -26,84 +27,90 @@ function next(tecla){
 function newLine(){
     let val = document.getElementById('input_text'+value).value;
     let command = document.getElementById('input_text'+value).value;
-    if(command=='dir'){
-        if(dir == 'rob'){
-        $('body').append('</br>15/02/2022ㅤㅤㅤㅤ14:08ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤ.android<br>11/02/2022ㅤㅤㅤㅤ19:03ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤBoot<br>11/02/2022ㅤㅤㅤㅤ19:03ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤdebug <br>');
-        }else{
-            if(dir == 'windows'){
+    switch(command){
+    //caso dir
+    case 'dir':
+        switch(dir){
+            case 'rob':
+                $('body').append('</br>15/02/2022ㅤㅤㅤㅤ14:08ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤ.android<br>11/02/2022ㅤㅤㅤㅤ19:03ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤBoot<br>11/02/2022ㅤㅤㅤㅤ19:03ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤdebug <br>');
+                break
+            case 'windows':
                 $('body').append('</br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤSystem32 <br>11/01/2022ㅤㅤㅤㅤ21:02ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤLogs <br>11/02/2022ㅤㅤㅤㅤ23:11ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤ<br>1#/^2/2?2@ㅤㅤㅤ%9:0@ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤX4nn*<br>');
-            }else{
-                if(dir == 'users'){
-                    $('body').append('</br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤRoberto');
-                }else{
-                    if(dir == 'c'){
-                        $('body').append('</br>11/02/2022ㅤㅤㅤㅤ10:21ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤProgram Files</br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤWindows</br>12/02/2022ㅤㅤㅤㅤ12:43ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤUsers <br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤSystem32 <br>');
-                    }
-                }
-            }
+                break
+            case 'users':
+                $('body').append('</br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤRoberto');
+                break
+            case 'c':
+                $('body').append('</br>11/02/2022ㅤㅤㅤㅤ10:21ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤProgram Files</br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤWindows</br>12/02/2022ㅤㅤㅤㅤ12:43ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤUsers <br>11/02/2022ㅤㅤㅤㅤ11:42ㅤㅤㅤㅤ&lt;DIR&gt;ㅤㅤㅤㅤSystem32 <br>');
+                break
         }
-    }else{
-    if(command=='cls'){
+        break
+    //caso cls
+    case 'cls':
         $('#body').empty();
-    }else{
-    if(command=='cd'){
+        break
+    //caso cd
+    case 'cd':
         $('body').append('</br>'+directoryw+'</br>');
-    }else{
-        // CD..
-    if(command=='cd..' || command == 'cd ..'){
-        if(dir == 'rob'){
-            directoryw = 'C:&bsol;Users';
-            directory = 'C:&bsol;Users&#62;';
-            dir = 'users';
-        }else{
-            if(dir=='users'){
+        break
+    //caso cd ..
+    case 'cd ..':
+    case 'cd..':
+        switch(dir){
+            case 'rob':
+                directoryw = 'C:&bsol;Users';
+                directory = 'C:&bsol;Users&#62;';
+                dir = 'users';
+                break
+            case 'windows':
                 directoryw = 'C:&bsol;';
                 directory = 'C:&bsol;&#62;';
                 dir = 'c';
-            }else{
-                if(dir == 'windows'){
+                break
+            case 'users':
                 directoryw = 'C:&bsol;';
                 directory = 'C:&bsol;&#62;';
                 dir = 'c';
-                }
-            }
+                break
         }
-    }else{
-
-    //cd
-
-    if(command=='cd Windows'){
+        break
+    //cd Windows
+    case 'cd Windows':
         if(dir == 'c'){
-        directory = 'C:&bsol;Windows&#62;';
-        dir = 'windows'
+            directory = 'C:&bsol;Windows&#62;';
+            dir = 'windows'
         }else{
             $('body').append('</br> El sistema no puede encontrar la ruta especificada.  </br>');
         }
-    }else{
-    if(command=='cd Users'){
+        break
+    //caso cd Users
+    case 'cd Users':
         if(dir == 'c'){
             directoryw = 'C:&bsol;Users';
             directory = 'C:&bsol;Users&#62;';
             dir = 'users';
+        }else{
+            $('body').append('</br> El sistema no puede encontrar la ruta especificada.  </br>');
         }
-    }else{
-        if(command == 'cd Roberto'){
-            if(dir == 'users');
+        break
+    //caso cd Roberto
+    case 'cd Roberto':
+        if(dir == 'users'){
             directoryw = 'C:&bsol;Users&bsol;Roberto';
             directory = 'C:&bsol;Users&bsol;Roberto&#62;';
             dir = 'rob';
-    }else{
-        
-    //texto vacio o no esperado
-    if(command==''){
-    }else{ 
+        }else{
+            $('body').append('</br> El sistema no puede encontrar la ruta especificada.  </br>');
+        }
+        break
+    default: 
         $('body').append('</br> "'+ val + '" no se reconoce como un comando interno o externo, programa o archivo por lotes ejecutable. </br>');
-    }}}}}}}}
-    value++
+        break
+    }
+    //final del switch
 
     //newline
+    value++
     var text = "</br>"+ directory +"<input type='text' class='typing' autocomplete='off' autofocus id='input_text" + value + "'>";        
     $("body").append(text); 
 }
-
-
