6a30ad9123d6d24e5ba89882a71bd8743f7c90d1
nicogarcia0 <nicogt712@gmail.com>
2022-04-05 18:42:12 +0200

    Cookies montaje

--- main/nivel 1/js/nivel1.js
+++ main/nivel 1/js/nivel1.js
@@ -189,30 +189,60 @@ function comprobación() {
     }
     else if (comprobador == secuencia && vecescorrecto == 3 && correcto == true && comprado == true) {
         switch(secuencia){
-            case "CCC":
-                if(disco==true){
-                    createCookie("disk","SamsungSSD")
-                    createCookie("tam","1TB")
-                    console.log("---------------"+"\n"+getCookie("disk")+"\n"+"---------------")
-                    console.log("---------------"+"\n"+getCookie("tam")+"\n"+"---------------")
-                    break
-                }
-            case "BBB":
-                if(disco==true){
-                    createCookie("disk","WesternDigital")
-                    createCookie("tam","1TB")
-                    console.log("---------------"+"\n"+getCookie("disk")+"\n"+"---------------")
-                    console.log("---------------"+"\n"+getCookie("tam")+"\n"+"---------------")
-                    break
-                }
             case "AAA":
-                if(disco==true){
-                    createCookie("disk","WesternDigital")
-                    createCookie("tam","500GB")
+                    createCookie("disk","WD 500GB")
+                    createCookie("tam","500 TB")
                     console.log("---------------"+"\n"+getCookie("disk")+"\n"+"---------------")
                     console.log("---------------"+"\n"+getCookie("tam")+"\n"+"---------------")
-                    break
-                }
+
+                    createCookie("gpu","GT 710")
+                    console.log("---------------"+"\n"+getCookie("gpu")+"\n"+"---------------")
+
+                    createCookie("cpu","I3")
+                    console.log("---------------"+"\n"+getCookie("cpu")+"\n"+"---------------")
+
+                    createCookie("ram","4 GB")
+                    console.log("---------------"+"\n"+getCookie("ram")+"\n"+"---------------")
+
+                    createCookie("placa","Pro Z690-A")
+                    console.log("---------------"+"\n"+getCookie("placa")+"\n"+"---------------")
+                    break;
+            case "BBB":
+                    createCookie("disk","WD 1TB")
+                    createCookie("tam","1TB")
+                    console.log("---------------"+"\n"+getCookie("disk")+"\n"+"---------------")
+                    console.log("---------------"+"\n"+getCookie("tam")+"\n"+"---------------")
+
+                    createCookie("gpu","RX 550")
+                    console.log("---------------"+"\n"+getCookie("gpu")+"\n"+"---------------")
+
+                    createCookie("cpu","I3")
+                    console.log("---------------"+"\n"+getCookie("cpu")+"\n"+"---------------")
+
+                    createCookie("ram","4 GB")
+                    console.log("---------------"+"\n"+getCookie("ram")+"\n"+"---------------")
+
+                    createCookie("placa","B550M -A PRO")
+                    console.log("---------------"+"\n"+getCookie("placa")+"\n"+"---------------")
+                    break;
+            case "CCC":
+                createCookie("disk","Samsung SSD 1TB")
+                createCookie("tam","1TB")
+                console.log("---------------"+"\n"+getCookie("disk")+"\n"+"---------------")
+                console.log("---------------"+"\n"+getCookie("tam")+"\n"+"---------------")
+
+                createCookie("gpu","Nvidia RTX 3060 ti")
+                console.log("---------------"+"\n"+getCookie("gpu")+"\n"+"---------------")
+
+                createCookie("cpu","Ryzen 9")
+                console.log("---------------"+"\n"+getCookie("cpu")+"\n"+"---------------")
+
+                createCookie("ram","16 GB")
+                console.log("---------------"+"\n"+getCookie("ram")+"\n"+"---------------")
+
+                createCookie("placa","B150 PLUS II")
+                console.log("---------------"+"\n"+getCookie("placa")+"\n"+"---------------")
+                break;
         }
         location.href = "./animacion1/animacion.html"
     }
