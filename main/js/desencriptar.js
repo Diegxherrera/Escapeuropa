519c05132fbf25a62414bb510f100472a0cd5931
nicogarcia0 <nicogt712@gmail.com>
2022-04-05 08:38:05 +0200

    Desencriptar

--- main/js/desencriptar.js
+++ main/js/desencriptar.js
@@ -1,5 +1,20 @@
 function desencriptar(){
-    document.getElementById('apptitle app-3-title').innerHTML = 'Desencriptado';
-    document.getElementById('appicon app-3-icon').src='img/decrypter.png';
-    console.log('work');
+    setTimeout(function() {
+        document.getElementById('apptitle app-3-title').innerHTML = 'Finding...';
+        document.getElementById('appicon app-3-icon').src='img/blocked.png';
+        setTimeout(function() {
+            document.getElementById('apptitle app-3-title').innerHTML = 'James D4cryIpkHtGerFIg=';
+            document.getElementById('appicon app-3-icon').src='img/help_icon.png';
+            setTimeout(function() {
+                document.getElementById('apptitle app-3-title').innerHTML = 'James D3c4yp73r';
+                document.getElementById('appicon app-3-icon').src='img/decrypter.png';
+                setTimeout(function() {
+                    document.getElementById('apptitle app-3-title').innerHTML = 'James Decrypter';
+                    document.getElementById('appicon app-3-icon').src='img/decrypter.png';
+                    }, 1000)
+                }, 1000)
+        }, 2000)
+    }, 0)
+
+
 }
