2a6dad791ae27b6d508e0e9adcdbd9634c75868d
nicogarcia0 <nicogt712@gmail.com>
2022-04-06 10:42:28 +0200

    temporizador reparado

--- main/js/contador.js
+++ main/js/contador.js
@@ -1,21 +1,22 @@
-let minuto = 0
-let segundos = 1
+var minuto = 0
+var segundos = 1
 let comprobador_minuto = 1
 let comprobador_segundo = 1
 setInterval(function() {
-    if (comprobador_segundo % 60 == 0) {
-        minuto = 1 * comprobador_minuto
-        segundos = 1
-        comprobador_minuto = comprobador_minuto + 1
-        comprobador_segundo = 1
-        if(segundo < 10){
-            document.getElementById("time").innerHTML = minuto +  ":" + '0' + segundos
+    if(segundos == 59){
+        document.getElementById('time').innerHTML = '0' + minuto + ':' + segundos
+        segundos = 0;
+        minuto++
+        if(minuto == 10){
+            document.location = "game_over.html"
         }
-    } else {
+    }else{
+        if(segundos <= 9 ){
+            document.getElementById('time').innerHTML = '0' + minuto + ':' + '0' + segundos
+            segundos++
+        }else{
+        document.getElementById('time').innerHTML = '0' + minuto + ':' + segundos
         segundos++
-        if(segundos < 10){
-            document.getElementById("time").innerHTML = minuto +  ":" + '0' + segundos
         }
-        comprobador_segundo++
     }
 }, 1000)
